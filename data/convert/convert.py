68fd8616efc48e502aa469e21796ef79ff7837e1
lukavdplas <l.p.vanderplas@uu.nl>
2022-03-01 14:01:19 +0100

    update tests

--- data/convert/convert.py
+++ data/convert/convert.py
@@ -1,3 +1,12 @@
+import json
+
 def convert_file(in_path, out_path):
+    data = parse_input(in_path)
+
     with open(out_path, 'w') as outfile:
-        outfile.write('')
+        json.dump(data, outfile)
+
+def parse_input(in_path):
+    return {
+        'test': {'name': 'test'}
+    }
--- data/convert/test_conversion.py
+++ data/convert/test_conversion.py
@@ -2,12 +2,39 @@ from lib2to3.pytree import convert
 import pytest
 from convert.convert import convert_file
 import os
+import json
 
 EXAMPLE_IN = 'example/example.xlsx'
 EXAMPLE_OUT = 'example/example.json'
+EXAMPLE_TARGET = 'example/example_target.json'
 
-def test_conversion(convert_example):
-    assert os.path.exists('example/example.json')
+
+def load_results():
+    with open(EXAMPLE_OUT) as input:
+        result = json.loads(input.read())
+    
+    with open(EXAMPLE_TARGET) as input:
+        target = json.loads(input.read())
+    
+    return result, target
+
+def check_table(table, result, target):
+    results = result[table]
+    targets = target[table]
+
+    assert len(results) == len(targets)
+
+    for result_item, target_item in zip(results, targets):
+        assert result_item == target_item
+
+def test_data(convert_example):
+    assert os.path.exists(EXAMPLE_OUT)
+    result, target = load_results()
+
+    for table in target:
+        assert table in result
+
+        check_table(table, result, target)
 
 @pytest.fixture
 def convert_example():
