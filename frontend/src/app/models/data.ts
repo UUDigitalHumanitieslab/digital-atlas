2e924962dc7b1fe25bebd9b92758406eda427e57
lukavdplas <l.p.vanderplas@uu.nl>
2022-03-07 15:48:58 +0100

    add data models

---
+++ frontend/src/app/models/data.ts
@@ -0,0 +1,63 @@
+export type WorkCategory = 'Work/biography'|'Media/lectures'|'Other';
+
+export interface Author {
+    name: string;
+    pictures?: string[];
+    placeOfBirth: string;
+    dateOfBirth: Date;
+    placeOfDeath?: string;
+    dateOfDeath?: Date;
+    description: string;
+}
+
+export interface Work {
+    author: string;
+    category: WorkCategory;
+    date?: Date;
+    startDate?: Date;
+    endDate?: Date;
+    pictures?: string[];
+    where?: string;
+    description: string;
+}
+
+export interface Legacy {
+    authorNames: string[];
+    about: string[];
+    category: WorkCategory;
+    date?: Date;
+    startDate?: Date;
+    endDate?: Date;
+    pictures?: string[];
+    where?: string;
+    title: string;
+    description: string;
+    url?: string;
+}
+
+export interface LifeEvent {
+    author: string;
+    category: WorkCategory;
+    date?: Date;
+    startDate?: Date;
+    endDate?: Date;
+    pictures?: string[];
+    where?: string;
+    title: string;
+    description: string;
+}
+
+export interface Location {
+    name: string;
+    lat: number;
+    long: number;
+}
+
+export interface Picture {
+    name: string;
+    filename: string;
+    author: string;
+    license: string;
+    url: string;
+    description: string;
+}
