327e982b1a67e7e050ed1b2f361ca827a5e36b9b
lukavdplas <l.p.vanderplas@uu.nl>
2022-04-08 15:26:42 +0200

    icons in timeline

--- frontend/src/app/timeline/timeline-event/timeline-event.component.ts
+++ frontend/src/app/timeline/timeline-event/timeline-event.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faBook, faLandmark, faUser } from '@fortawesome/free-solid-svg-icons';
 import { TimelineEvent } from 'src/app/models/timeline';
 
 @Component({
-  selector: 'da-timeline-event',
-  templateUrl: './timeline-event.component.html',
-  styleUrls: ['./timeline-event.component.scss']
+    selector: 'da-timeline-event',
+    templateUrl: './timeline-event.component.html',
+    styleUrls: ['./timeline-event.component.scss']
 })
 export class TimelineEventComponent implements OnInit {
-  @Input() event: TimelineEvent;
+    icons = {
+        'life event': faUser,
+        work: faBook,
+        legacy: faLandmark
+    };
 
-  constructor() { }
+    @Input() event: TimelineEvent;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
