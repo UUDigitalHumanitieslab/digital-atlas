0b443c63a390408a9f724bb3204014ad46a41ef9
lukavdplas <l.p.vanderplas@uu.nl>
2022-05-02 11:45:55 +0200

    include authors in event cards

--- frontend/src/app/event-card/event-card.component.ts
+++ frontend/src/app/event-card/event-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { faCalendar, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { Author, CollectedData, Legacy, LifeEvent, Work } from '../models/data';
 import { DatesService } from '../services/dates.service';
 import { VisualService } from '../services/visual.service';
@@ -11,11 +12,15 @@ import { VisualService } from '../services/visual.service';
 export class EventCardComponent implements OnInit, OnChanges {
     @Input() event: LifeEvent|Work|Legacy;
     @Input() data: CollectedData;
+    @Input() includeAuthor = true;
 
     authors: Author[];
     picture: string;
     formattedDate: string;
     color: string;
+
+    faCalendar = faCalendar;
+    faMapMarker = faMapMarkerAlt;
 
     constructor(private datesService: DatesService, private visualService: VisualService) { }
 
@@ -28,8 +33,18 @@ export class EventCardComponent implements OnInit, OnChanges {
 
             this.picture = this.visualService.getPicture(this.event, this.data);
             this.color = this.visualService.getColor(this.event, this.data);
-        }
 
+            if (this.includeAuthor) {
+                const authorIds = (this.event as Legacy).aboutIds || [(this.event as LifeEvent|Work).authorId];
+                this.authors = authorIds.map(id => this.data.authors.find(author => author.id === id));
+            } else {
+                this.authors = [];
+            }
+        }
+    }
+
+    getPicture(author: Author): string {
+        return this.visualService.getPicture(author, this.data);
     }
 
 }
