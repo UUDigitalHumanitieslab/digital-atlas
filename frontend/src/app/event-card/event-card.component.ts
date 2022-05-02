02734caaee77bb3729708b51b4eaa97d9fb16724
lukavdplas <l.p.vanderplas@uu.nl>
2022-05-02 13:45:31 +0200

    get data from dataService

--- frontend/src/app/event-card/event-card.component.ts
+++ frontend/src/app/event-card/event-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faCalendar, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { Author, CollectedData, Legacy, LifeEvent, Work } from '../models/data';
+import { DataService } from '../services/data.service';
 import { DatesService } from '../services/dates.service';
 import { VisualService } from '../services/visual.service';
 
@@ -11,7 +12,7 @@ import { VisualService } from '../services/visual.service';
 })
 export class EventCardComponent implements OnInit, OnChanges {
     @Input() event: LifeEvent|Work|Legacy;
-    @Input() data: CollectedData;
+    data: CollectedData;
     @Input() includeAuthor = true;
 
     authors: Author[];
@@ -22,13 +23,15 @@ export class EventCardComponent implements OnInit, OnChanges {
     faCalendar = faCalendar;
     faMapMarker = faMapMarkerAlt;
 
-    constructor(private datesService: DatesService, private visualService: VisualService) { }
+    constructor(private dataService: DataService, private datesService: DatesService, private visualService: VisualService) { }
 
     ngOnInit(): void {
     }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        if (this.event && this.data) {
+    async ngOnChanges(changes: SimpleChanges): Promise<void> {
+        if (this.event) {
+            this.data = await this.dataService.getData();
+
             this.formattedDate = this.datesService.formatEventDate(this.event);
 
             this.picture = this.visualService.getPicture(this.event, this.data);
