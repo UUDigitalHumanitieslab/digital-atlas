59bb2420918663a11a02b8c5750d168d3ac1e601
Sheean Spoel <sheean@sheean.nl>
2022-05-06 10:19:15 +0200

    Zoom and pan the map

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -7,8 +7,33 @@ import * as _ from 'underscore';
 import { VisualService } from '../services/visual.service';
 
 const worldPath = '/assets/data/world-atlas-110m.json';
+
+const circleRadius = 5;
 const width = 962;
 const height = 550;
+const scaleExtent: [number, number] = [0.27, 3.5];
+const coords = {
+    topLeft: {
+        long: -160,
+        lat: 80
+    },
+
+    bottomRight: {
+        long: 170,
+        lat: - 76
+    },
+
+    center: {
+        long: 0,
+        lat: 55
+    }
+}
+
+type PointLocation = {
+    where: Location,
+    color: string,
+    event: LifeEvent | Work | Legacy
+};
 
 @Component({
     selector: 'da-map',
@@ -18,7 +43,10 @@ const height = 550;
 export class MapComponent implements OnInit, OnChanges {
     private svg: any;
     private projection: d3.GeoProjection;
+    private zoom: d3.ZoomBehavior<Element, unknown>;
     private mapReady: Promise<void>;
+
+    zoomFactor = 1;
 
     @Input() data: CollectedData;
     points: any = undefined;
@@ -26,7 +54,8 @@ export class MapComponent implements OnInit, OnChanges {
     @ViewChild('target')
     target: ElementRef<SVGElement>;
 
-    selectedEvent: LifeEvent|Work|Legacy;
+    selectedEvent: LifeEvent | Work | Legacy;
+    pointLocations: PointLocation[];
 
     constructor(private visualService: VisualService) { }
 
@@ -38,19 +67,17 @@ export class MapComponent implements OnInit, OnChanges {
         if (this.data) {
             await this.mapReady;
 
-
             const allEvents = _.flatten([
                 _.map(this.data.lifeEvents, this.locationFromEvent, this),
                 _.map(this.data.works, this.locationFromEvent, this),
                 _.map(this.data.legacies, this.locationFromEvent, this)
             ]);
-            const eventsWithLocation = allEvents.filter(event => event.where);
-            this.drawPoints(eventsWithLocation);
+            this.pointLocations = allEvents.filter(event => event.where);
+            this.drawPoints();
         }
     }
 
-    private locationFromEvent(event: LifeEvent|Work|Legacy):
-        {where: Location, color: string, event: LifeEvent|Work|Legacy} {
+    private locationFromEvent(event: LifeEvent | Work | Legacy): PointLocation {
         return {
             where: event.where,
             color: this.visualService.getColor(event, this.data),
@@ -62,14 +89,13 @@ export class MapComponent implements OnInit, OnChanges {
         const world = await d3.json<any>(worldPath);
 
         const projection = d3.geoMercator()
-            .scale(500)
-            .translate([width / 2.5, height / 0.65]);
+            .scale(400);
 
         const svg = d3.select(this.target.nativeElement)
             .attr('width', '100%')
-            .attr('height', `${(height / width) * 100}vw`)
-            .attr('viewBox', `0 0 ${width} ${height}`)
-            ;
+            // warning! this reads the current height outside of this component
+            .attr('height', document.getElementsByClassName('main-content')[0].clientHeight)
+            .attr('viewBox', `0 0 ${width} ${height}`);
 
         const path = d3.geoPath()
             .projection(projection);
@@ -88,26 +114,69 @@ export class MapComponent implements OnInit, OnChanges {
 
         this.svg = svg;
         this.projection = projection;
+
+        this.setZoom();
+        const [x, y] = this.projection([
+            coords.center.long,
+            coords.center.lat]);
+        this.zoom.translateTo(this.svg, x, y);
     }
 
-    private async drawPoints(locations: { where: Location, color: string, event: LifeEvent|Work|Legacy }[]): Promise<any> {
+    private setZoom() {
+        const self = this;
+        function handleZoom(e: any) {
+            d3.select('svg g')
+                .attr('transform', e.transform);
+
+            // update size of points
+            self.zoomFactor = e.transform.k;
+            d3.select('svg g').selectAll('circle')
+                .attr('r', circleRadius / self.zoomFactor);
+        }
+
+        const topLeft = this.projection([
+            coords.topLeft.long,
+            coords.topLeft.lat
+        ]);
+        const bottomRight = this.projection([
+            coords.bottomRight.long,
+            coords.bottomRight.lat
+        ]);
+
+        this.zoom = d3.zoom()
+            .on('zoom', handleZoom)
+            .scaleExtent(scaleExtent)
+            .translateExtent([topLeft, bottomRight]);
+
+        d3.select(this.target.nativeElement)
+            .call(this.zoom);
+    }
+
+    private async drawPoints(): Promise<void> {
         if (this.points) {
             this.points.remove();
         }
-        this.points = this.svg.selectAll('circle')
-            .data(locations)
+        this.points = this.svg.select('g').selectAll('circle')
+            .data(this.pointLocations)
             .enter()
             .append('circle')
             .attr('cx', (d: { where: Location }) => this.projection([d.where.long, d.where.lat])[0])
             .attr('cy', (d: { where: Location }) => this.projection([d.where.long, d.where.lat])[1])
-            .attr('r', 5)
+            .attr('r', circleRadius / this.zoomFactor)
             .attr('fill', (d: { color: string }) => colors[d.color])
             .attr('stroke-width', 0)
             .on('click', this.showEventCard.bind(this));
     }
 
-    showEventCard(clickEvent, obj): void {
+    showEventCard(clickEvent, obj: { event: MapComponent['selectedEvent']}): void {
         this.selectedEvent = obj.event;
+        const [x, y] = this.projection([
+            obj.event.where.long,
+            obj.event.where.lat]);
+        
+        this.svg.transition()
+            .duration(750)
+            .call(this.zoom.translateTo, x, y);
     }
 
     hideEventCard(): void {
